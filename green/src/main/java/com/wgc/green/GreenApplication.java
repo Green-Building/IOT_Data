8532b3f7e1fdd6774df8e9b92944521f717d2eb2
7seven7lst <shengtaoli0110@gmail.com>
2018-12-07 19:57:12 -0800

    [update] update

--- green/src/main/java/com/wgc/green/GreenApplication.java
+++ green/src/main/java/com/wgc/green/GreenApplication.java
@@ -2,11 +2,8 @@ package com.wgc.green;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class GreenApplication {
 
 	public static void main(String[] args) {
